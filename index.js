f136f6e9776bfb6a018f382eabf8bf6903330eb4
Ender Hosting <jonasdefarias@hotmail.com>
2019-06-26 12:39:50 -0300

    Limpesa de código

--- index.js
+++ index.js
@@ -28,72 +28,65 @@ db.ref('/registrar').on('value', (data) => {
     registrar = data.val();
 });
 
-
-app.get('/api/registrar',(req, res) => {
-    res.send(JSON.stringify(registrar));
-});
+/*
+* checado 26/jun/2019
+*/
+app.get('/api/registrar',(req, res) => res.send(JSON.stringify(registrar)));
 
 //Listar todos os cadastros
 app.get('/api/cadastros/', (req, res) => {
     db.ref("/registros/").once('value').then((data) => {
-        console.log(data.val());
+        console.log(`HTTP get ${req.path}: `, data.val());
         res.send(data.val());
     });
 });
 
-app.get('/api/log/', (req, res) => {
-    db.ref('/log').once('value').then((data) => {
-        res.send(data.val());
-    });
-});
 
-//Listar um cadastro especifico
-app.get('/api/cadastros/:id', (req, res) => {
-    db.ref("/registros/" + req.params.id).once('value').then((data) => {
-        if(!data.val()) return res.status(404).send("Não encontrado");
-        res.send(data.val());
-    });
-});
 
-//Registrar um ID unico || Usado pelo Leitor biometrico durante o cadastro
+/*
+ * Registrar um ID unico || Usado pelo Leitor biometrico durante o cadastro
+ * checado 26/jun/2019
+ */
 app.post('/api/registrar/:id', (req, res) => {
     console.log("Recebido registro ID: " + req.params.id);
     db.ref('/registros/' + req.params.id + '/').update({
-        id: req.params.id
+        digital: true
     });
     db.ref("/").update({
         registrar: parseInt(0)
     });
-    res.send("ok");
+    res.send("cadastro ativado...");
 });
 
-/*
-    Registrar por ID unico
+/* 
+* NOVO METODO DE REGISTRO 11-JUN-2019
+* checado 26/jun/2019
 */
-app.post('/api/registrar/:id/:name/:ra/:aluno/:curso', (req, res) => {
-    const { error } = validar(res.params);
-    console.log(req.params);
-    if(error) return res.status(400).send(error.details[0].message);
-    if(!req.params.ra){
-        req.params.ra = 0;
-    }
-   
-    db.ref('/registros/' + req.params.id + '/').set({
-        id: req.params.id,
-        name: req.params.name,
-        ra: req.params.ra,
-        aluno: req.params.aluno === 'academico' ? true : false,
-        curso: req.params.curso
+app.post('/api/registrar', (req, res) => {
+    console.log(`HTTP post from ${req.path} body:`, req.body);
+
+    db.ref('/registros').once('value').then(value => {
+
+        res.setHeader('Content-Type', 'application/json');
+        let data = {};
+        let nextIdRegitro = 1;
+        while(value.hasChild(`${nextIdRegitro}`))
+            nextIdRegitro++;
+
+        db.ref("/").update({ registrar: nextIdRegitro })
+            .then(() => {
+                data = {...data, id: nextIdRegitro, digital: false, ...req.body.cadastro};
+                
+                db.ref(`/registros/${nextIdRegitro}/`).update(data);
+                res.send(JSON.stringify(data));
+        });
     });
-    
-    res.send({
-        complete: "okay",
-        id: req.params.id
-    });
-});
+}); 
+
 
 /*
     Quando um usuario acessa o laboratorio com a digital
+    checado 26/jun/2019
 */
 app.post('/api/senddata/:id', (req, res) => {
     if(!req.params.id) return res.status(400).send("ID não foi definido");
@@ -103,7 +96,7 @@ app.post('/api/senddata/:id', (req, res) => {
         id: req.params.id
     });
     db.ref("registros/" + req.params.id).once('value').then(data => {
-        console.log("Bem vindo " + (data.val().ra == 0 ? 'Prof. ' : '')  + data.val().name);
+        console.log("Bem vindo " + (!data.val().ra ? 'Prof. ' : '')  + data.val().username);
     });
     res.status(200).send("okay");
 });
@@ -122,44 +115,16 @@ app.post('/api/abrircadastro/:id', (req, res) => {
     });
 });
 
-
-
 /*
-    Atualização de cadastro
+* checado 26/Jun/2019
 */
-app.put('/api/cadastro/:id', (req, res) => {
-    if(!req.params.id) return res.status(400).send("ID é obrigatório");
-    const { error } = validar(req.query);
-    if(error) return res.status(400).send(error.details[0].message);
-    console.log(req.query);
-    let isAluno = (req.query.aluno === 'true' ? true : false);
-    db.ref('/registros/' + req.params.id + "").update({
-        ra: !isAluno ? null : req.query.RA,
-        name: req.query.name,
-        aluno: isAluno,
-        curso: req.query.curso
-    });
-    res.send(req.query);
-});
-
 app.get('/api/getmensagem/:msgname', (req, res) => {
     if(!req.params.msgname) return res.status(400).send("id da mensagem é obrigatório");
     db.ref('mensagens/' + req.params.msgname).once('value').then(data => {
-        console.log(data.val());
+        console.log(`HTTP get ${req.path}: `,data.val());
         res.send(data.val());
     });
 });
 
 const port = 80;
 app.listen(port, () => console.log("Listening on port", port));
-
-
-function validar(cadastro){    
-    const schemaName = {
-        name: Joi.string().min(5).required(),
-        RA: Joi.optional(),
-        aluno: Joi.optional(),
-        curso: Joi.optional()
-    };
-    return Joi.validate(cadastro, schemaName);
-}
